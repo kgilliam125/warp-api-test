c1fe8313f09d48b38a5ad7898c706998d3240eed
unknown <kyle@moonmantis.gg>
2024-08-07 22:43:41 -0700

    Add route for list of todos

--- src/handler.rs
+++ src/handler.rs
@@ -1,7 +1,5 @@
 use crate::{
-    model::Todo,
-    response::{GenericResponse, SingleTodoResponse, TodoData, TodoListResponse},
-    WebResult, DB,
+    model::Todo, response::{GenericResponse, SingleTodoResponse, TodoData, TodoListResponse}, schema::QueryOptions, WebResult, DB
 };
 use chrono::prelude::*;
 use uuid::Uuid;
@@ -20,5 +18,22 @@ pub async fn health_check_handler() -> WebResult<impl Reply> {
     };
 
     Ok(json(response))
+}
+
+pub async fn todo_list_handler(opts: QueryOptions, db: DB) -> WebResult<impl Reply> {
+    let todos = db.lock().await;
+
+    let limit = opts.limit.unwrap_or(10);
+    let offset = (opts.page.unwrap_or(1) -1) * limit;
+
+    let todos: Vec<Todo> = todos.clone().into_iter().skip(offset).take(limit).collect();
+
+    let json_response = TodoListResponse {
+        status: "success".to_string(),
+        results: todos.len(),
+        todos
+    };
+    
+    Ok(json(&json_response))
 }
 
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod response;
 mod schema;
 
 use model::DB;
-use warp::{Filter, Rejection};
+use warp::{Filter, Rejection, http::Method};
 
 type WebResult<T> = Result<T, Rejection>;
 
@@ -17,13 +17,30 @@ async fn main() {
     }
     pretty_env_logger::init();
 
-    // let db = model::init_todo_db();
+    let db = model::init_todo_db();
+
+    let todo_router = warp::path!("api" / "todos");
+
+    let cors = warp::cors()
+    .allow_methods(&[Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+    .allow_origins(vec!["http://localhost:3000/", "http://localhost:8000/"])
+    .allow_headers(vec!["content-type"])
+    .allow_credentials(true);
 
     let health_route = warp::path!("api" / "health")
         .and(warp::get())
         .and_then(handler::health_check_handler);
 
-    let routes = health_route.with(warp::log("api"));
+    let todo_routes = todo_router
+        .and(warp::get())
+        .and(warp::query::<schema::QueryOptions>())
+        .and(with_db(db.clone()))
+        .and_then(handler::todo_list_handler);
+
+    let routes = health_route
+        .with(cors)
+        .with(warp::log("api"))
+        .or(todo_routes);
 
     println!("Starting server");
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
